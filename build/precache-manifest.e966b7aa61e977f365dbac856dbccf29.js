986dfe0b8f8c1375a56aa8cd5ae9783273e70df5
mixmaster10 <smart.topdev007@gmail.com>
2021-07-26 22:04:07 +0300

    added netlify.toml

---
+++ build/precache-manifest.e966b7aa61e977f365dbac856dbccf29.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b9875000e4d18c2129c771261d0ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf884591e35ed614da4",
+    "url": "/static/css/main.4631e0de.chunk.css"
+  },
+  {
+    "revision": "f3c99df3829efd9c85ea",
+    "url": "/static/js/2.3ee87afa.chunk.js"
+  },
+  {
+    "revision": "7d9a1e97b63fb04c3f7226116d9275de",
+    "url": "/static/js/2.3ee87afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf884591e35ed614da4",
+    "url": "/static/js/main.7b6ceacc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e798d67774817bb757333f08bc192f89",
+    "url": "/static/media/2nd_registration_step1.e798d677.svg"
+  },
+  {
+    "revision": "d32d0bc6055e717d19b6c1bd38805a86",
+    "url": "/static/media/background2.d32d0bc6.png"
+  },
+  {
+    "revision": "1dea58283016462fd566f43a632ff80f",
+    "url": "/static/media/circles.1dea5828.png"
+  },
+  {
+    "revision": "d5219990e302cc87aa442125e617307f",
+    "url": "/static/media/jackpot_.d5219990.png"
+  },
+  {
+    "revision": "5b46f7b12a403f92f71254646e6db577",
+    "url": "/static/media/jackpot_white.5b46f7b1.png"
+  },
+  {
+    "revision": "708c670ec0f0b7c076e960b7b02e6e21",
+    "url": "/static/media/left_arrow.708c670e.png"
+  },
+  {
+    "revision": "40b7f71a33edffaa0e0bbef53de0116e",
+    "url": "/static/media/logo-.40b7f71a.svg"
+  },
+  {
+    "revision": "4a6aed4fcaf4e77061131af49f3e539d",
+    "url": "/static/media/nlogo.4a6aed4f.png"
+  },
+  {
+    "revision": "465e85a4b82048876f187e0debe4ec35",
+    "url": "/static/media/right_arrow.465e85a4.png"
+  },
+  {
+    "revision": "5ef7d8c8d8f697a9fa456b4b90762940",
+    "url": "/static/media/top_arrow.5ef7d8c8.png"
+  }
+]);
